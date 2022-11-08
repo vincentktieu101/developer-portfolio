7fa9d5afa00383251f3c335e27ae2f3ffa8bba0d
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2022-11-08 00:24:51 -0800

    finished contact info links

--- sections/Contact/ContactInfo.tsx
+++ sections/Contact/ContactInfo.tsx
@@ -8,8 +8,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 
 import SectionHeader from "@components/Section/SectionHeader";
-
-// TODO: have to add links properly
+import ExternalLink from "@components/Link/ExternalLink";
 
 function ContactInfo() {
   return (
@@ -24,30 +23,77 @@ function ContactInfo() {
       >
         <SectionHeader underlined>More Info</SectionHeader>
         <div style={{ maxWidth: "100%" }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <ExternalLink
+            href="https://www.google.com/maps/place/Rosemead,+CA/@34.064641,-118.1168945,13z/data=!3m1!4b1!4m5!3m4!1s0x80c2d074766c77b1:0xa0351d489643887c!8m2!3d34.0805651!4d-118.072846"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <HomeIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>Rosemead, Los Angeles</a>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
+            Rosemead, Los Angeles
+          </ExternalLink>
+          <ExternalLink
+            href="https://www.google.com/search?q=6265375590&source=hp&ei=LttzYIS5Icvb-gSCrYiYCA&iflsig=AINFCbYAAAAAYHPpPhPl2um35fphoMiN7ljN2XEbmanf&oq=6265375590&gs_lcp=Cgdnd3Mtd2l6EAM6AggAOg4ILhCxAxCDARDHARCjAjoICAAQsQMQgwE6CAguELEDEIMBOgUIABCxAzoFCC4QsQM6CwguELEDEIMBEJMCOg4ILhCxAxCDARDHARCvAToICC4QxwEQrwE6BAgAEB46BwgAEMkDEB46BggAEAUQHlD8BlioJWCnJ2gCcAB4AIABdYgBqQmSAQM2LjaYAQCgAQGqAQdnd3Mtd2l6sAEA&sclient=gws-wiz&ved=0ahUKEwiEiMyZ_ffvAhXLrZ4KHYIWAoMQ4dUDCAg&uact=5"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <PhoneIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>(626) 537-5590</a>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
+            (626) 537-5590
+          </ExternalLink>
+          <ExternalLink
+            href="mailto:vincentktieu101@gmail.com"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <MailIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>vincentktieu101@gmail.com</a>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
+            vincentktieu101@gmail.com
+          </ExternalLink>
+          <ExternalLink
+            href="https://github.com/vincentktieu101"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <GithubIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>https://github.com/vincentktieu101</a>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
+            https://github.com/vincentktieu101
+          </ExternalLink>
+          <ExternalLink
+            href="https://www.linkedin.com/in/vincentktieu"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <LinkedInIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>https://www.linkedin.com/in/vincentktieu</a>
-          </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
+            https://www.linkedin.com/in/vincentktieu
+          </ExternalLink>
+          <ExternalLink
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "10px",
+              color: "black",
+            }}
+          >
             <PostAddIcon fontSize="large" style={{ marginRight: "10px" }} />
-            <a>Resume</a>
-          </div>
+            Resume
+          </ExternalLink>
         </div>
       </div>
     </div>
