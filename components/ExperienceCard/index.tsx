358a9d9e8cacb6a29aaf1312d598e0f704a667ae
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2022-11-19 15:03:34 -0800

    remove styles from experiencecard

--- components/ExperienceCard/index.tsx
+++ components/ExperienceCard/index.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import WorkIcon from "@mui/icons-material/Work";
 import SchoolIcon from "@mui/icons-material/School";
 
-import {
-  BottomAccent,
-  TypeIcon,
-} from "./styles";
-
 interface ExperienceCardProps {
   type: string;
   title: string;
@@ -19,18 +14,16 @@ interface ExperienceCardProps {
   text: string;
 }
 
-function ExperienceCard(props: ExperienceCardProps) {
-  const {
-    type,
-    title,
-    company,
-    primaryColor = "black",
-    secondaryColor = "black",
-    location,
-    time,
-    text,
-  } = props;
-
+function ExperienceCard({
+  type,
+  title,
+  company,
+  primaryColor = "black",
+  secondaryColor = "black",
+  location,
+  time,
+  text,
+}: ExperienceCardProps) {
   // TODO: move this to utils folder
   const copyToClipBoard = (primaryColor: string) => {
     navigator.clipboard.writeText(primaryColor);
@@ -41,10 +34,10 @@ function ExperienceCard(props: ExperienceCardProps) {
     <div className="w-full max-w-[900px] overflow-hidden mb-[20px] border-[1px] rounded-md border-gray-100 shadow-md">
       <div className="flex">
         <div className="bg-color-white w-[80px]">
-          <TypeIcon className="flex justify-center items-center w-[60px] h-[60px] rounded-md" primaryColor={primaryColor} secondaryColor={secondaryColor}>
+          <div className="flex justify-center items-center w-[60px] h-[60px] rounded-md" style={{backgroundColor: primaryColor, color: secondaryColor}}>
             {type === "work" && <WorkIcon fontSize="large" />}
             {type === "school" && <SchoolIcon fontSize="large" />}
-          </TypeIcon>
+          </div>
         </div>
         <div className="w-full p-[20px]">
           <div className="text-[36px] font-bold leading-[1.2]">{title}</div>
@@ -54,11 +47,11 @@ function ExperienceCard(props: ExperienceCardProps) {
           {text && <p className="leading-2 mt-[20px]">{text}</p>}
         </div>
       </div>
-      <BottomAccent className="flex justify-center text-[10px] h-[15px] w-full bg-[${primaryColor}] text-transparent hover:text-white" primaryColor={primaryColor}
+      <div className="flex justify-center text-[10px] h-[15px] w-full bg-[${primaryColor}] text-transparent hover:text-white" style={{backgroundColor: primaryColor}}
         onClick={() => copyToClipBoard(primaryColor)}
       >
         company color: {primaryColor}
-      </BottomAccent>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,6 @@ module.exports = {
     extend: {
       animation: {},
       keyframes: {},
-      backgroundColor: {
-        // ziprecruiter
-        "#277f6a": "#277f6a",
-        "#b2e522": "#b2e522",
-        // syntiant
-        "#162c44": "#162c44",
-        // ucsb
-        "#00539b": "#00539b",
-        "#ffc323": "#ffc323",
-        // ugig
-        "#1976d2": "#1976d2",
-        "#46bdef": "#46bdef",
-      },
     },
   },
   plugins: [],
