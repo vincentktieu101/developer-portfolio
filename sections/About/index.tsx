fca761c9bdc07234f55ec920bc336f63c0dc6e3b
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2022-11-19 23:23:29 -0800

    refactor about section

--- sections/About/index.tsx
+++ sections/About/index.tsx
@@ -3,32 +3,24 @@ import React from "react";
 import Link from "next/link";
 
 import Container from "@mui/material/Container";
-// import ExternalLink from "@components/Link/ExternalLink";
 import Section from "@components/Section";
 
 import UCSBBirdEyesView from "@images/backgrounds/ucsb-birdseyeview.jpg";
 import resumeLink from "@objects/resume-link";
 import SectionHeader from "@components/Section/SectionHeader";
 
-import {
-  MainContentFlexBox,
-  Summary,
-  StyledUCSBBirdEyesView,
-  B,
-} from "./styles";
-
 const FirstParagraph = () => (
-  <p>
-    Hi! I'm <B>Vincent Tieu,</B> a <B>software engineer</B> and{" "}
-    <B>full-stack web developer,</B> based in <B>Los Angeles!</B> I am
+  <p className="leading-2 mb-[20px]">
+    Hi! I'm <b className="font-bold">Vincent Tieu,</b> a <b className="font-bold">software engineer</b> and{" "}
+    <b className="font-bold">full-stack web developer,</b> based in <b className="font-bold">Los Angeles!</b> I am
     passionate about programming, whether that means starting a web project from
     scratch (like this online portfolio!) or developing an existing codebase.
   </p>
 );
 
 const SecondParagraph = () => (
-  <p>
-    I'm a new graduate from <B>UC Santa Barbara</B> in <B>Computer Science</B>!
+  <p className="leading-2 mb-[20px]">
+    I'm a new graduate from <b className="font-bold">UC Santa Barbara</b> in <b className="font-bold">Computer Science</b>!
     I enjoy programming, from designing and developing frontends, building
     RESTful APIs, and optimizing backend operations. I'm most comfortable in the
     React and NodeJS frameworks.
@@ -36,7 +28,7 @@ const SecondParagraph = () => (
 );
 
 const ThirdParagraph = () => (
-  <p>
+  <p className="leading-2 mb-[20px]">
     Below, you can view my professional experience as well as some personal
     projects. For a quick summary, check out my {/* Should be Link Component */}
     <Link href={resumeLink}>
@@ -48,7 +40,7 @@ const ThirdParagraph = () => (
 
 const PhotoCaption = () => (
   <p>
-    View of My Alma Mater, <B>UC Santa Barbara</B>
+    View of My Alma Mater, <b className="font-bold">UC Santa Barbara</b>
   </p>
 );
 
@@ -56,18 +48,18 @@ function About() {
   return (
     <Section id="about">
       <Container>
-        <MainContentFlexBox>
-          <Summary>
+        <div className="flex flex-col items-center gap-[40px] lg:flex-row lg:justify-center">
+          <div className="max-w-[450px] lg:max-w-[450px]">
             <SectionHeader underlined>About Me</SectionHeader>
             <FirstParagraph />
             <SecondParagraph />
             <ThirdParagraph />
-          </Summary>
+          </div>
           <div>
-            <StyledUCSBBirdEyesView src={UCSBBirdEyesView.src} />
+            <img src={UCSBBirdEyesView.src} className="max-h-[440px] mb-[10px]"/>
             <PhotoCaption />
           </div>
-        </MainContentFlexBox>
+        </div>
       </Container>
     </Section>
   );
