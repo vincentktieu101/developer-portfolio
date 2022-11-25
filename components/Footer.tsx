fcca15ce2460c5562f00721401645a2353ff6d1e
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2022-11-25 02:56:32 -0800

    removed tailwind css on mui component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import Container from "@mui/material/Container";
 import ExternalLink from "./Link/ExternalLink";
 
 function Footer() {
   return (
     <footer className="bg-black text-white pt-[10px] pb-[50px]">
-      <Container className="flex flex-col items-center">
+      <div className="flex flex-col items-center">
         <p>© {new Date().getFullYear()}, VINCENT&apos;S DEVELOPER PORTFOLIO</p>
         <p>
           Built with
@@ -23,7 +22,7 @@ function Footer() {
           </ExternalLink>
           .
         </p>
-      </Container>
+      </div>
     </footer>
   );
 }
